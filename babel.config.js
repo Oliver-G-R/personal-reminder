311350407094bbd298ed1c3275bed6156cfa4d1d
Oliver Guerrero Ruiz <oliver.kali.hub@gmail.com>
2022-05-14 17:22:51 -0500

    fix: alias for babelrc

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['module-resolver', {
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@context': './src/context',
+          '@Types': './src/Types',
+          '@hooks': './src/hooks',
+          '@helpers': './src/helpers'
+        }
+      }]
+    ]
   }
 }
